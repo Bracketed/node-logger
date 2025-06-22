cf78c1fdf2fae0b9bd67e7ab93e73cc0338def3a
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2025-06-22 23:12:24 +0100

    1.1.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,32 +1,3 @@
-import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
-import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
-import { defineConfig, type Options } from 'tsup';
+import { packageConfiguration } from '@bracketed/tsup-configuration';
 
-const baseOptions: Options = {
-	clean: true,
-	entry: ['src/**/*.ts'],
-	dts: true,
-	minify: false,
-	skipNodeModulesBundle: true,
-	sourcemap: true,
-	target: 'es2021',
-	tsconfig: 'tsconfig.json',
-	keepNames: true,
-	esbuildPlugins: [esbuildPluginVersionInjector(), esbuildPluginFilePathExtensions()],
-	treeshake: true,
-};
-
-export default [
-	defineConfig({
-		...baseOptions,
-		outDir: 'lib/cjs',
-		format: 'cjs',
-		outExtension: () => ({ js: '.cjs' }),
-	}),
-	defineConfig({
-		...baseOptions,
-		outDir: 'lib/esm',
-		format: 'esm',
-	}),
-];
-
+export default [...packageConfiguration];
